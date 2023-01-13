a6ffc09ae8f25d762d822f6bbca82c09fa1008ac
Armen Mirzoyan Denisov <100298950+armirzoya@users.noreply.github.com>
2023-01-13 09:53:30 +0100

    Initial commit

--- gateway/src/main/java/com/cap/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/cap/gateway/GatewayApplication.java
@@ -2,10 +2,8 @@ package com.cap.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
